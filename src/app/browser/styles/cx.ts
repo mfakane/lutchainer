4f2ebf3b4bb7b5be73b178aeb6f93bd193786fc2
mfakane <509998+mfakane@users.noreply.github.com>
2026-04-18 20:18:12 +0000

    chore: remove unused file

--- src/app/browser/styles/cx.ts
+++
@@ -1,3 +0,0 @@
-export function cx(...classNames: Array<string | false | null | undefined>): string {
-  return classNames.filter((value): value is string => typeof value === 'string' && value.length > 0).join(' ');
-}
