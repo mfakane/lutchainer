cf8dd82f837cd5a7559e2745c16e78745f346f21
mfakane <509998+mfakane@users.noreply.github.com>
2026-04-19 10:14:28 +0000

    refactor: make CLI version command promise-aware

--- src/app/cli/commands/version-command.ts
+++ src/app/cli/commands/version-command.ts
@@ -1,9 +1,9 @@
 import { getBuildCommitId } from '../cli-build-info.ts';
 import { PROCESS_CLI_IO, type CliIo } from '../cli-io.ts';
 
-export async function runVersionCommand(argv: string[], io: CliIo = PROCESS_CLI_IO): Promise<number> {
+export function runVersionCommand(argv: string[], io: CliIo = PROCESS_CLI_IO): Promise<number> {
   const revision = getBuildCommitId();
   void argv;
   io.stdout(`${revision}\n`);
-  return 0;
+  return Promise.resolve(0);
 }
